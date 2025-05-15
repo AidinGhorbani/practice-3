ce726dee8c0e9a7d5d8879011bc0aec40f87a89f
Aidin Ghorbani <aidinghorbani89@gmail.com>
2025-05-15 22:05:20 +0330

    Add files via upload

---
+++ aidin3.py
@@ -0,0 +1,12 @@
+a = ["0919" , '0938' , '0939']
+b = ['0919' , '0935' , '0936']
+e = a + b
+e = set(e)
+print(e)
+# ---------------------------
+d = {}
+k = input('word:')
+m = input('meaning:')
+m2 = input('meaning2:')
+d[k] = [m, m2]
+print(d)
